f0e26d59865175bbcf3093565960cc0c29ffbf19
Varun Munjeti <vrunjeti@gmail.com>
2016-10-08 18:37:16 +0800

    list of current tabs

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 chrome.management.getSelf(extensionInfo => console.log('extensionInfo', extensionInfo))
 
-chrome.tabs.create({ url: 'http://www.google.com' }, tab => console.log('tab', tab))
+chrome.windows.getAll({ populate: true }, windows => {
+  console.log('windows', windows)
+
+  const allTabs = windows.reduce((tabs, currWindow) => {
+    const currWindowTabs = currWindow.tabs
+    tabs.push(...currWindowTabs)
+    return tabs
+  }, [])
+  console.log('allTabs', allTabs)
+
+  chrome.tabs.get(allTabs[0].id, tabInfo => console.log('tabInfo', tabInfo))
+})
+
+// chrome.tabs.create({ url: 'http://www.google.com' }, tab => console.log('tab', tab))
